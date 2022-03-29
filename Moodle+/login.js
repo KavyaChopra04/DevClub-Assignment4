40fc0605f5a626fa455a712ee9af2ad6c815038f
Kavya <chopra.kavya04@gmail.com>
2022-03-29 22:32:57 +0530

    First

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,31 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.getElementById('login'); // Fill the selector for the login element in ""
 let login_text = login_element.innerText;
-
-let question = ""; // Use split and array operations on the login_text string to extract the question
-
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
-
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+let arr=login_text.split('\n')
+let question = arr[3]; // Use split and array operations on the login_text string to extract the question
+let answer = "";
+let arr1=question.split(' ')
+if(question.includes('second'))
+{
+    let num2=parseInt(arr1[6]);
+    answer=num2;
+}
+if(question.includes('first'))
+{
+    let num1=parseInt(arr1[4]);
+    answer=num1;
+}
+if(question.includes('add'))
+{
+    let num1=parseInt(arr1[2]);
+    let num2=parseInt(arr1[4]);
+    answer=num1+num2;
+}
+if(question.includes('subtract'))
+{
+    let num1=parseInt(arr1[2]);
+    let num2=parseInt(arr1[4]);
+    answer=num1-num2;
+}// Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+const captcha_input_element = document.getElementById('valuepkg3'); // Fill the selector for the captcha input element in ""
 captcha_input_element.value = answer;
--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,16 +1,35 @@
+const { EFAULT } = require("constants");
 const http = require("http");
+const { getDefaultSettings } = require("http2");
 
-const SECRET = "CIGAR"; // You can set any word as the secret answer
+var SECRET = "CIGAR"; // You can set any word as the secret answer
 
 function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
-
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
+	if(req.url=='/favicon.ico')
+	{
+		return
+	}
+	let arr = req.url.split('?q=')
+    const guess = arr[1]; // Write logic to parse the word which the user guessed from the URL string
+	console.log(guess)
+	let feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	for(let i=0; i<5;i++)
+	{
+		if(guess[i]==SECRET[i])
+		{
+			feedback+='g'
+		}
+		else if(guess[i]!=SECRET[i]&&SECRET.includes(guess[i]))
+		{
+			feedback+='y'
+		}
+		else{
+			feedback+='b'
+		}
+	}
 	res.write(feedback);
 	res.end();
 }
-
 http.createServer(myFunction).listen(8080);
